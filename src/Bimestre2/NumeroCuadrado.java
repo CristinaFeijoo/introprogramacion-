7669e2e0e8e687944a3fff81b8ff3ece94113fb7
CristinaFeijoo <thatfoxy1245@gmail.com>
2022-12-17 16:13:36 -0500

    ArreglosNumeros

---
+++ src/Bimestre2/NumeroCuadrado.java
@@ -0,0 +1,12 @@
+package Bimestre2;
+
+public class NumeroCuadrado {
+    public static void main(String[] args) {
+        int numeros [] = {2,3,5,7,9,11,13};
+        int tam_n = numeros.length;
+        for (int indice = 0; indice <tam_n ; indice++) {
+            int n_cuadrado = (numeros[indice]*numeros[indice]);
+            System.out.println((numeros[indice])+" al cuadrado = "+(n_cuadrado));
+        }
+    }
+}
